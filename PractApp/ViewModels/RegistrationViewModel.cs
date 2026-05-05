341414d6faca76ea817bf42a5d85471fc2497ae4
Practic <Practic@DESKTOP-BDLPUCP>
2026-05-05 15:20:14 +0300

    db

--- PractApp/ViewModels/RegistrationViewModel.cs
+++ PractApp/ViewModels/RegistrationViewModel.cs
@@ -1,61 +1,66 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Npgsql;
 using PractApp.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PractApp.ViewModels
 {
     public partial class RegistrationViewModel : ViewModelBase
     {
-        private readonly string _filePath = "user.csv";
+        private readonly string _connectionString = "Host=pg4.sweb.ru;Port=5433;Database=wmpzbebra2Username=wmpzbebra2;Password=LUdmila29";
 
         public static List<User> users = new List<User>();
 
-        [ObservableProperty] 
-        private string _regLogin = "";
-        [ObservableProperty] 
-        private string _regEmail = "";
-        [ObservableProperty] 
-        private string _regPassword = "";
         [ObservableProperty]
-        private string _message = "";
+        private string? _regLogin;
+        [ObservableProperty]
+
+        private string? _regEmail;
+        [ObservableProperty]
+        private string? _regPassword;
+        [ObservableProperty]
+        private string? _message;
+
+
+        
+
+      
+
 
         [RelayCommand]
-        private void Register()
+        private async Task RegisterAsync()
         {
-            if(string.IsNullOrWhiteSpace(RegLogin) || string.IsNullOrWhiteSpace(RegEmail) || string.IsNullOrWhiteSpace(RegPassword))
-            {
-                Message = "Заполните поля";
-                return;
-            }
+            string passHash = BCrypt.Net.BCrypt.HashPassword(RegPassword);
+
             try
             {
-                var newUser = new User
-                {
-                    Login = RegLogin,
-                    Password = RegPassword,
-                    Email = RegEmail,
-                };
+                using var conn = new NpgsqlConnection(_connectionString);
+                await conn.OpenAsync();
 
-                string csvLine = newUser.toCsv() + Environment.NewLine;
-                File.AppendAllText(_filePath, csvLine);
+                using var cmd = new NpgsqlCommand("INSERT INTO user (login, password_hash, email) VALUES (@login, @password, @email)", conn);
 
-                RegEmail = "";
-                RegLogin = "";
-                RegPassword = "";
+                cmd.Parameters.AddWithValue("login", RegLogin!);
+                cmd.Parameters.AddWithValue("email", RegEmail ?? (object)System.DBNull.Value);
+                cmd.Parameters.AddWithValue("password", passHash);
+
+                await cmd.ExecuteNonQueryAsync();
+                Message = "Регистрация успешна!";
             }
-            catch(Exception ex)
+            catch (PostgresException ex) when (ex.SqlState == "23505") 
             {
-                Message = $"Ошибка: {ex.Message}";
+                Message = "Этот логин уже занят.";
             }
-
-
-
-                
+            catch
+            {
+                Message = "Ошибка подключения к базе.";
+            }
         }
     }
 }
